06c30cbd36fff7e948c100fde21a6bc66b211566
Kumar <600913@Cognizant.com>
2017-09-18 18:43:48 +0530

    added-towers-disks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Tower from './Tower';
 import Disk from './Disk';
+import towerimg from '../assets/images/towerpeg.png';
 
 class App extends Component {
 
@@ -23,10 +24,20 @@ var lists =[];
     return (
       <div className="app">
         <h1>Hello, Curious Five!!!</h1>
+        <div style={{display:'inline-block', paddingLeft:'200px',width:'250px', height:'350px',position:'relative', top: '-31px', right:'27px'}}>
            <ul style={{listStyle: 'none', width:'400px'}}  >
          {this.createDiskList()}
-            </ul>       
-        <Tower/>
+            </ul>     
+            </div>  
+        <div style={{display:'inline-block',  padding:'25px', width:'250px', height:'350px', backgroundSize: 'cover', backgroundImage:"url(" + towerimg + ")"}}>
+         
+          </div>
+            <div style={{display:'inline-block', padding:'25px' , width:'250px', height:'350px' ,backgroundSize: 'cover', backgroundImage:"url(" + towerimg + ")"}}>
+         
+          </div>
+            <div style={{display:'inline-block' , padding:'25px' ,width:'250px', height:'350px',backgroundSize: 'cover',backgroundImage:"url(" + towerimg + ")"}}>
+        
+          </div>
       </div>
     );
   }
